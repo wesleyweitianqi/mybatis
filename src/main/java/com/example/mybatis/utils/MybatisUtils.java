90735f85e0bf584aa6753d812e4f8cc0f7b0009c
wesleyweitianqi <wesleytianqiwei@gmail.com>
2023-07-19 23:28:59 -0400

    add mybatisUtils

---
+++ src/main/java/com/example/mybatis/utils/MybatisUtils.java
@@ -0,0 +1,33 @@
+package com.example.mybatis.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class MybatisUtils {
+  private static SqlSessionFactory sqlSessionFactory = null;
+  static {
+    Reader reader = null;
+    try {
+      reader = Resources.getResourceAsReader("mybatis-config.xml");
+      sqlSessionFactory = new SqlSessionFactoryBuilder().builder(reader);
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new ExceptionInInitializerError(e);
+    }
+  }
+
+  public static SqlSession openSession() {
+    return sqlSessionFactory.openSession();
+  }
+
+  public static void closeSession(SqlSession session) {
+    if (session != null) {
+      session.close();
+    }
+  }
+}
--- src/test/java/com/example/mybatis/AppTest.java
+++ src/test/java/com/example/mybatis/AppTest.java
@@ -2,19 +2,35 @@ package com.example.mybatis;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.Reader;
+import java.sql.Connection;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSqlSessionFactory() {
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        System.out.println("SessionFactory加载成功");
+        SqlSession sqlSession = null;
+        try {
+
+            sqlSession = sqlSessionFactory.openSession();
+
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+
+                sqlSession.close();
+            }
+        }
     }
 }
